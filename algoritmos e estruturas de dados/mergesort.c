c414c119026a1ea6e1c6815a8833c45e44b2a528
Helter7 <50884379+Helter7@users.noreply.github.com>
2019-11-25 20:59:23 -0300

    Update mergesort.c

--- algoritmos e estruturas de dados/mergesort.c
+++ algoritmos e estruturas de dados/mergesort.c
@@ -1,5 +1,6 @@
 //agorithm merge sort with another structure as the item
 #include<stdio.h>
+#include<string.h>
 typedef struct Carro{
     char* placa[8];
     char* marca[15];
@@ -7,25 +8,26 @@ typedef struct Carro{
     int ano;
 }carro;
 
+int compare_carro(carro lista[],int i, int k)
+{
+    return strcmp(lista[i].placa,lista[k].placa);
+}
+
 void exibir(carro lista[],int tam)
 {
     int i;
     for(i=0;i<tam;i++){
-        printf("%d|",lista[i].);
+        printf("%s| %s| %s| %d\n",lista[i].marca,
+                                lista[i].modelo,
+                                lista[i].placa,
+                                lista[i].ano);
     }
-}
-//function of swap items
-void swap(int *a,int *b)
-{
-    int aux;
-    aux = *a;
-    *a = *b;
-    *b = aux;
 }
 //recursive function merge sort
 void mergesort_rec(carro lista[],int ini, int fim)
 {
-    int pos=(ini+fim)/2,i=ini,j=pos+1,k=0,lista_aux[fim-ini+1];
+    int pos=(ini+fim)/2,i=ini,j=pos+1,k=0;
+    carro lista_aux[fim-ini+1];
 
     if(ini<fim)
     {
@@ -42,7 +44,7 @@ void mergesort_rec(carro lista[],int ini, int fim)
                 lista_aux[k]=lista[i];
                 i++;
             }
-            else if(lista[i]<lista[j]){
+            else if(compare_carro(lista,i,j) < 0){
                 lista_aux[k]=lista[i];
                 i++;
             }
@@ -67,7 +69,29 @@ void mergesort(carro lista[],int tam)
 
 main()
 {
-    int lista[7]={5,8,4,9,2,7,98} , tam=7;
+    carro carro1,carro2,carro3;
+    int tam = 3;
+    carro lista[3];
+
+    strcpy(carro1.placa,"C"); //LPP8977
+    strcpy(carro1.modelo,"Focus");
+    carro1.ano = 2010;
+    strcpy(carro1.marca,"Ford");
+
+    strcpy(carro2.placa,"A"); //KMP9826
+    strcpy(carro2.marca,"Volkswagen");
+    strcpy(carro2.modelo,"Polo");
+    carro2.ano = 2008;
+
+    strcpy(carro3.placa,"B"); //AMP0927
+    strcpy(carro3.marca,"Chevrolet");
+    strcpy(carro3.modelo,"Corsa");
+    carro3.ano=2012;
+
+    lista[0] = carro1;
+    lista[1] = carro2;
+    lista[2] = carro3;
+
     printf("\nUnordered List\n");
     exibir(lista,tam);
     mergesort(lista,tam);
